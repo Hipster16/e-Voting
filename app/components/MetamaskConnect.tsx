fd481daaa5f6f14a0c6fb8ebdfabbb1fac556965
hipster16 <vinu.b.kurup@gmail.com>
2024-05-31 11:18:24 +0530

    fix: seprated client component from main page

---
+++ app/components/MetamaskConnect.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+export default function MetamaskConnect() {
+    const handleConnect = () => {
+        //Logic For metamask connection comes here...
+    }
+  return (
+    <button className="bg-blue-600 text-lg font-extrabold py-4 px-10 rounded-full hover:bg-black" onClick={handleConnect}>
+        Connect Wallet
+    </button>
+  )
+}
--- app/login/admin/page.tsx
+++ app/login/admin/page.tsx
@@ -1,18 +1,14 @@
+import MetamaskConnect from "@/app/components/MetamaskConnect";
 import React from "react";
 
 function AdminLogin() {
-    const handleConnect = () => {
-            //Logic For metamask connection comes here...
-    }
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className="w-[400px] h-min bg-white border-1 flex flex-col justify-between item-center gap-10 p-10 rounded-2xl">
         <p className="text-2xl text-center text-black font-extrabold">
           Connect to metamask
         </p>
-        <button className="bg-blue-600 text-lg font-extrabold py-4 px-10 rounded-full hover:bg-black" onClick={handleConnect}>
-          Connect Wallet
-        </button>
+        <MetamaskConnect/>
       </div>
     </div>
   );
