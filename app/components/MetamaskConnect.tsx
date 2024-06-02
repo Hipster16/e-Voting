294f81163aa8c7ff8eaab66af71e28e6aabf2a72
hipster16 <vinu.b.kurup@gmail.com>
2024-06-02 19:03:29 +0530

    fix: hydration error fixed for connect button

--- app/components/MetamaskConnect.tsx
+++ app/components/MetamaskConnect.tsx
@@ -6,8 +6,10 @@ export default function MetamaskConnect() {
         //Logic For metamask connection comes here...
     }
   return (
-    <button className="bg-blue-600 text-lg font-extrabold py-4 px-10 rounded-full hover:bg-black" onClick={handleConnect}>
-        Connect Wallet
-    </button>
+    <>
+      <button className="bg-blue-600 text-lg font-extrabold py-4 px-10 rounded-full hover:bg-black" onClick={handleConnect}>
+          Connect Wallet
+      </button>
+    </>
   )
 }
