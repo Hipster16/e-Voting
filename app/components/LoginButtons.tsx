14741fb87a8419af89e117e602aa8b145406d8e9
hipster16 <vinu.b.kurup@gmail.com>
2024-06-26 20:53:40 +0530

    style: homepage login page visible only if user not signed in

--- app/components/LoginButtons.tsx
+++ app/components/LoginButtons.tsx
@@ -1,32 +1,64 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+"use client";
+import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import auth from "@/firebase/auth";
+
 
 export default function LoginButtons() {
-    const [visible, setVisible] = useState(false);
-    const router = useRouter();
-     
-    const handleClick = () => {
-        setVisible(true);
-    }
-    
-    const handleAdminLogin = () => {
-        router.push('/login/admin');
-    }
+  const [visible, setVisible] = useState(false);
+  const [logggedIn, setLogggedIn] = useState(true);
+  const router = useRouter();
 
-    const handleStudentLogin = () => {
-        router.push('/login/student');
-    }
-  return (
-    <div className='w-[30%]'>
-        {!visible ?
-            <button className='bg-blue-600 text-xl font-semibold py-4 px-10 rounded-full hover:bg-white hover:text-blue-600' onClick={handleClick}>Login</button>
-            :
-            <div className='w-full flex justify-between'>
-                <button className='bg-blue-600 text-xl font-semibold py-4 px-10 rounded-full hover:bg-white hover:text-blue-600' onClick={handleAdminLogin}>Admin</button>
-                <button className='bg-blue-600 text-xl font-semibold py-4 px-10 rounded-full hover:bg-white hover:text-blue-600' onClick={handleStudentLogin}>student</button>
-            </div>
-        }
-    </div>
-  )
+  const handleClick = () => {
+    setVisible(true);
+  };
+
+  const handleAdminLogin = () => {
+    router.push("/login/admin");
+  };
+
+  const handleStudentLogin = () => {
+    router.push("/login/student");
+  };
+
+  useEffect(() => {
+    const unsbscribe = onAuthStateChanged(auth.authState, (user) => {
+      if (user) {
+        setLogggedIn(true);
+      } else {
+        setLogggedIn(false);
+      }
+    });
+    return unsbscribe;
+  });
+  if (!logggedIn) {
+    return (
+      <div className="w-[30%]">
+        {!visible ? (
+          <button
+            className="bg-blue-600 text-xl font-semibold py-4 px-10 rounded-full hover:bg-white hover:text-blue-600"
+            onClick={handleClick}
+          >
+            Login
+          </button>
+        ) : (
+          <div className="w-full flex justify-between">
+            <button
+              className="bg-blue-600 text-xl font-semibold py-4 px-10 rounded-full hover:bg-white hover:text-blue-600"
+              onClick={handleAdminLogin}
+            >
+              Admin
+            </button>
+            <button
+              className="bg-blue-600 text-xl font-semibold py-4 px-10 rounded-full hover:bg-white hover:text-blue-600"
+              onClick={handleStudentLogin}
+            >
+              student
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
