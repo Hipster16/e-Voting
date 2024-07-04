ee5ab742da1508a1dadb127aa71df564dbe41c61
hipster16 <vinu.b.kurup@gmail.com>
2024-07-04 18:23:01 +0530

    feat: add home page link to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -101,7 +101,7 @@ export default function Navbar() {
 
   return (
     <div className="w-full h-[100px] bg-slate-100/15 text-white flex justify-between items-center p-10 rounded-full border-1">
-      <h1 className="font-semibold text-xl">
+      <h1 className="font-semibold text-xl" onClick={() => router.push("/")}>
         <span className="text-blue-600">e</span>Voting
       </h1>
       {poppup}
